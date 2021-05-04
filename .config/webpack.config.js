d1ea7ca943c399ecd7df95d80488b2461aa9218a
James Sizeland <jamessizeland@gmail.com>
2021-05-04 23:16:58 +0100

    adds webpack and linting setup

---
+++ .config/webpack.config.js
@@ -0,0 +1,55 @@
+// https://betterprogramming.pub/6-webpack-concepts-for-advanced-react-developers-d016da2cad52
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.tsx"),
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        use: ["babel-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/index.html"),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "./src"),
+    port: 3000,
+    hotOnly: true,
+    compress: true,
+    open: true,
+  },
+};
