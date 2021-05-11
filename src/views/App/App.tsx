cb5da39abd514d604c6938fe035a0b0fd834b581
James Sizeland <jamessizeland@gmail.com>
2021-05-11 23:20:08 +0100

    adds useReducer example and useContext

--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import UseStateExampleComponent from '../../components/useStateExample';
+import Counter from '../../reducers/useReducerExample';
 // import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import style from './App.module.scss';
+import style from './App.module.scss'; // css-module import
 
 const App = (): JSX.Element => {
   return (
     <div>
       <p className={style.example}>Hello World from React</p>
       <UseStateExampleComponent />
+      <Counter />
     </div>
   );
 };
