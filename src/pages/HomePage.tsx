5522c04e2d88e81b1592988720827bc7737538a9
James Sizeland <jamessizeland@gmail.com>
2021-12-19 21:02:42 +0000

    add example header and footer components

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const HomePage = () => (
-  <div>
-    <h1 className="text-3xl font-bold underline">Home Page</h1>
+  <div className="">
+    <h1 className="text-3xl font-bold underline text-center">Home Page</h1>
   </div>
 );
