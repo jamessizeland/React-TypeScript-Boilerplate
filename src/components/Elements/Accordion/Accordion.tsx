bc8681542ffcc308078b35a089a014898e3b069f
James Sizeland <jamessizeland@gmail.com>
2021-12-19 22:18:07 +0000

    add accordion structure

--- src/components/Elements/Accordion/Accordion.tsx
+++ src/components/Elements/Accordion/Accordion.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Accordion() {
+type AccordionProps = {
+  children: JSX.Element;
+  id?: string;
+  title?: string;
+};
+
+export function Accordion({
+  children,
+  id = 'accordion',
+  title = '',
+}: AccordionProps): JSX.Element {
+  const [active, setActive] = useState(false);
+
   return (
-    <div className="accordion" id="accordionExample">
-      <div className="accordion-item bg-white border border-gray-200">
-        <h2 className="accordion-header mb-0" id="headingOne">
-          <button
-            className="
+    <div className="accordion-item bg-white border border-gray-200">
+      <h2 className="accordion-header mb-0" id={id}>
+        <button
+          className="
           accordion-button
           relative
           flex
@@ -21,124 +32,26 @@ export default function Accordion() {
           transition
           focus:outline-none
         "
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapseOne"
-            aria-expanded="true"
-            aria-controls="collapseOne"
-          >
-            Accordion Item #1
-          </button>
-        </h2>
-        <div
-          id="collapseOne"
-          className="accordion-collapse collapse show"
-          aria-labelledby="headingOne"
-          data-bs-parent="#accordionExample"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapseOne"
+          aria-expanded={active}
+          aria-controls="collapseOne"
+          onClick={() => {
+            setActive(!active);
+            console.log(active);
+          }}
         >
-          <div className="accordion-body py-4 px-5">
-            <strong>This is the first items accordion body.</strong> It is shown
-            by default, until the collapse plugin adds the appropriate classes
-            that we use to style each element. These classes control the overall
-            appearance, as well as the showing and hiding via CSS transitions.
-            You can modify any of this with custom CSS or overriding our default
-            variables. Its also worth noting that just about any HTML can go
-            within the <code>.accordion-body</code>, though the transition does
-            limit overflow.
-          </div>
-        </div>
-      </div>
-      <div className="accordion-item bg-white border border-gray-200">
-        <h2 className="accordion-header mb-0" id="headingTwo">
-          <button
-            className="
-          accordion-button
-          collapsed
-          relative
-          flex
-          items-center
-          w-full
-          py-4
-          px-5
-          text-base text-gray-800 text-left
-          bg-white
-          border-0
-          rounded-none
-          transition
-          focus:outline-none
-        "
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapseTwo"
-            aria-expanded="false"
-            aria-controls="collapseTwo"
-          >
-            Accordion Item #2
-          </button>
-        </h2>
-        <div
-          id="collapseTwo"
-          className="accordion-collapse collapse"
-          aria-labelledby="headingTwo"
-          data-bs-parent="#accordionExample"
-        >
-          <div className="accordion-body py-4 px-5">
-            <strong>This is the second items accordion body.</strong> It is
-            hidden by default, until the collapse plugin adds the appropriate
-            classes that we use to style each element. These classes control the
-            overall appearance, as well as the showing and hiding via CSS
-            transitions. You can modify any of this with custom CSS or
-            overriding our default variables. Its also worth noting that just
-            about any HTML can go within the <code>.accordion-body</code>,
-            though the transition does limit overflow.
-          </div>
-        </div>
-      </div>
-      <div className="accordion-item bg-white border border-gray-200">
-        <h2 className="accordion-header mb-0" id="headingThree">
-          <button
-            className="
-          accordion-button
-          collapsed
-          relative
-          flex
-          items-center
-          w-full
-          py-4
-          px-5
-          text-base text-gray-800 text-left
-          bg-white
-          border-0
-          rounded-none
-          transition
-          focus:outline-none
-        "
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapseThree"
-            aria-expanded="false"
-            aria-controls="collapseThree"
-          >
-            Accordion Item #3
-          </button>
-        </h2>
-        <div
-          id="collapseThree"
-          className="accordion-collapse collapse"
-          aria-labelledby="headingThree"
-          data-bs-parent="#accordionExample"
-        >
-          <div className="accordion-body py-4 px-5">
-            <strong>This is the third items accordion body.</strong> It is
-            hidden by default, until the collapse plugin adds the appropriate
-            classes that we use to style each element. These classes control the
-            overall appearance, as well as the showing and hiding via CSS
-            transitions. You can modify any of this with custom CSS or
-            overriding our default variables. Its also worth noting that just
-            about any HTML can go within the <code>.accordion-body</code>,
-            though the transition does limit overflow.
-          </div>
-        </div>
+          {title}
+        </button>
+      </h2>
+      <div
+        id={id}
+        className="accordion-collapse collapse show"
+        // aria-labelledby="headingOne"
+        // data-bs-parent="#accordionExample"
+      >
+        <div className="accordion-body py-4 px-5">{children}</div>
       </div>
     </div>
   );
